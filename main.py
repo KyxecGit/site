8c5552c54da802e77fc43f12701056c8cca46b41
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-05-07 17:58:17 +0600

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from db import get_question_after, get_quises, check_answer
 def start_quiz(quiz_id):
     session['quiz'] = quiz_id
     session['last_question'] = 0
-    session['answer'] = 0
+    session['answers'] = 0
     session['total'] = 0
 
 def end_quiz():
@@ -26,6 +26,13 @@ def index():
         return redirect(url_for('test'))
     
 
+def save_answer():
+    answer = request.form.get('ans_text')
+    quest_id =  request.form.get('q_id')
+    session['last_question'] += 1
+    if check_answer(quest_id,answer):
+        session['answers'] += 1
+
 def question_form(question):
     answer_list = [
         question[2], question[3], question[4], question[5]
@@ -38,17 +45,18 @@ def test():
         return redirect(url_for('index'))
     else:
         if request.method == 'POST':
-            #save_answer()
-            pass
+            save_answer()
         next_question = get_question_after(session['last_question'], session['quiz'])
         if next_question is None or len(next_question) == 0:
             return redirect(url_for('result'))
         else:
             return question_form(next_question)
-    
-
+        
 def result():
-    return '<h1>Здесь будет результат</h1>'
+    html = render_template('result.html', right = session['answers'], total = session['total'])
+    end_quiz()
+    return html
+    
 
 folder = os.getcwd()
 
