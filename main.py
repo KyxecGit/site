f3d1ef30fde9a5b9e8ca899fe96f80aaceda1a57
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-05-11 15:39:47 +0600

    Update main.py

--- main.py
+++ main.py
@@ -24,15 +24,31 @@ def index():
       quest_id = request.form.get('quiz')
       start_quiz(quest_id)
       return redirect(url_for('test'))
+   
+def question_form(question):
+   answers_list = [question[2],question[3],question[4],question[5]]
+   shuffle(answers_list)
+   return render_template('test.html',question=question[1],quest_id=question[0],answers_list=answers_list)
+
+
 
 def test():
-   result = get_question_after(session['last_question'], session['quiz'])
-   if result is None or len(result) == 0:
-       return redirect(url_for('result'))
+   if not ('quiz' in session) or int(session['quiz']) < 0:
+      return redirect(url_for('index'))
    else:
-       session['last_question'] = result[0]
-       # если мы научили базу возвращать Row или dict, то надо писать не result[0], а result['id']
-       return '<h1>' + str(session['quiz']) + '<br>' + str(result) + '</h1>'
+      if request.method == 'POST':
+         pass
+         #save_answers()
+      next_question = get_question_after(session['last_question'], session['quiz'])
+      if next_question is None or len(next_question) == 0:
+         return redirect(url_for('result'))
+      else:
+         return question_form(next_question)
+
+
+
+
+
 
 def result():
    return "that's all folks!"
@@ -41,7 +57,7 @@ folder = os.getcwd()
 # Создаём объект веб-приложения:
 app = Flask(__name__,template_folder=folder,static_folder=folder) 
 app.add_url_rule('/', 'index', index, methods=['post','get'])   # создаёт правило для URL '/'
-app.add_url_rule('/test', 'test', test) # создаёт правило для URL '/test'
+app.add_url_rule('/test', 'test', test, methods=['post','get']) # создаёт правило для URL '/test'
 app.add_url_rule('/result', 'result', result) # создаёт правило для URL '/test'
 # Устанавливаем ключ шифрования:
 app.config['SECRET_KEY'] = 'ThisIsSecretSecretSecretLife'
