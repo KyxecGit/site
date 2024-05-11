deb2080858e3e39ca168819e0ea10295752f53f1
KyxecGit <107474711+KyxecGit@users.noreply.github.com>
2024-05-11 14:20:43 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,71 +1,39 @@
-import os
-from random import shuffle
-from flask import Flask , session, request , redirect, render_template, url_for
-from db import get_question_after, get_quises, check_answer
+from random import randint
+from flask import Flask, session, redirect, url_for
+from db_scripts import get_question_after
 
-def start_quiz(quiz_id):
-    session['quiz'] = quiz_id
-    session['last_question'] = 0
-    session['answers'] = 0
-    session['total'] = 0
 
-def end_quiz():
-    session.clear()
-
-def quiz_form():
-    q_list = get_quises()
-    return render_template('index.html', q_list = q_list)
-
+​
 def index():
-    if request.method == 'GET':
-        start_quiz(-1)
-        return quiz_form()
-    else:
-        quest_id = request.form.get('quiz')
-        start_quiz(quest_id)
-        return redirect(url_for('test'))
-    
-
-def save_answer():
-    answer = request.form.get('ans_text')
-    quest_id =  request.form.get('q_id')
-    session['last_question'] += 1
-    if check_answer(quest_id,answer):
-        session['answers'] += 1
-
-def question_form(question):
-    answer_list = [
-        question[2], question[3], question[4], question[5]
-    ]
-    shuffle(answer_list)
-    return render_template('test.html',question=question[1], question_id= question[0], answer_list=answer_list)
-
+   max_quiz = 3
+   # или если ученик написал get_quiz_count(), то можно её импортировать и указать:
+   # max_quiz = get_quiz_count[0] 
+   session['quiz'] = randint(1, max_quiz)
+   # или если ученик написал get_random_quiz_id(), то можно её импортировать и указать:
+   # session['quiz'] = get_random_quiz_id()
+   session['last_question'] = 0
+   return '<a href="/test">Тест</a>'
+​
 def test():
-    if not ('quiz' in session) or int(session['quiz']) < 0:
-        return redirect(url_for('index'))
-    else:
-        if request.method == 'POST':
-            save_answer()
-        next_question = get_question_after(session['last_question'], session['quiz'])
-        if next_question is None or len(next_question) == 0:
-            return redirect(url_for('result'))
-        else:
-            return question_form(next_question)
-        
+   result = get_question_after(session['last_question'], session['quiz'])
+   if result is None or len(result) == 0:
+       return redirect(url_for('result'))
+   else:
+       session['last_question'] = result[0]
+       # если мы научили базу возвращать Row или dict, то надо писать не result[0], а result['id']
+       return '<h1>' + str(session['quiz']) + '<br>' + str(result) + '</h1>'
+​
 def result():
-    html = render_template('result.html', right = session['answers'], total = session['total'])
-    end_quiz()
-    return html
-    
-
-folder = os.getcwd()
-
-app = Flask(__name__,template_folder=folder,static_folder=folder)
-
-app.add_url_rule('/','index',index, methods=['post','get'])
-app.add_url_rule('/test','test',test, methods=['post','get'])
-app.add_url_rule('/result','result',result)
-
-app.config['SECRET_KEY'] = 'QWERTYUIOPOIUYTREWERTYUIOOIU'
-
-app.run()
+   return "that's all folks!"
+​
+# Создаём объект веб-приложения:
+app = Flask(__name__) 
+app.add_url_rule('/', 'index', index)   # создаёт правило для URL '/'
+app.add_url_rule('/test', 'test', test) # создаёт правило для URL '/test'
+app.add_url_rule('/result', 'result', result) # создаёт правило для URL '/test'
+# Устанавливаем ключ шифрования:
+app.config['SECRET_KEY'] = 'ThisIsSecretSecretSecretLife'
+​
+if __name__ == '__main__':
+   # Запускаем веб-сервер:
+   app.run()
